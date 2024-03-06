1e3df5d9bbee2b520052e913ccf99f7bfc648f83
Wisp <1343347+wispborne@users.noreply.github.com>
2024-03-05 16:09:11 -0800

    Update TemplateModPlugin.java

--- src/author/modname/TemplateModPlugin.java
+++ src/author/modname/TemplateModPlugin.java
@@ -9,7 +9,7 @@ public class TemplateModPlugin extends BaseModPlugin {
         super.onApplicationLoad();
 
         // Test that the .jar is loaded and working, using the most obnoxious way possible.
-        throw new RuntimeException("Template mod loaded! Remove this crash in TemplateModPlugin.");
+        throw new RuntimeException("Template mod loaded!\nRemove this crash in TemplateModPlugin.");
     }
 
     @Override
